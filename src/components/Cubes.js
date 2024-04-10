f6153b2d97fb93236fe24dc1373019d15d0207b1
DucSkii <ducvietdao@live.co.uk>
2024-04-10 21:10:47 +0100

    removed log

--- src/components/Cubes.js
+++ src/components/Cubes.js
@@ -3,7 +3,6 @@ import Cube from './Cube'
 
 const Cubes = () => {
   const [cubes] = useStore((state) => [state.cubes])
-  console.log('cubes', cubes)
 
   return cubes.map(({ key, pos, texture }) => {
     return <Cube key={key} cubeKey={key} position={pos} texture={texture} />
